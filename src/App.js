d4b36aacd8fbab067cc697f9ce710abaa50805ee
rebecca112233 <rebecca.shi@nyu.edu>
2019-04-11 19:59:36 -0400

    remove bootstrap navbar

--- src/App.js
+++ src/App.js
@@ -10,15 +10,6 @@ const App = () => (
     <div className="App">
       <div id="nav">
         <CustomNavbar />
-        {/* <Navbar fixed="top" bg="dark">
-          <Navbar.Brand href="#home">
-            Rebecca
-          </Navbar.Brand>
-          <Nav.Link href="#" style={{color:'white'}}>Home</Nav.Link>
-          <Nav.Link href="#" style={{color:'white'}}>About</Nav.Link>
-          <Nav.Link href="#" style={{color:'white'}}>Resume</Nav.Link>
-          <Nav.Link href="#" style={{color:'white'}}>Portfolio</Nav.Link>
-        </Navbar> */}
       </div>
       <div id="pageWrapper">
         <HomePage />
