6c10aaf11e982f34a0100cf8c9c6907655f6802e
rebecca112233 <rebecca.shi@nyu.edu>
2019-04-23 16:56:44 -0400

    cut unnecessary code from navbar file

--- src/components/CustomNavbar/CustomNavbar.js
+++ src/components/CustomNavbar/CustomNavbar.js
@@ -14,29 +14,16 @@ export default class CustomNavbar extends React.Component {
             dropdownOpen: (this.state.dropdownOpen === false) ? true : false
         })
     }
+
     render(){
-        return this.state.dropdownOpen ?
-        (
+        let dropdown_class = this.state.dropdownOpen ? "main-nav": "main-nav-gone";
+        return(
             <div id="customNav">
                 <span className="navbar-toggle" id="js-navbar-toggle">
                     <i className="fas fa-bars" onClick={this.dropdownOnClick}></i>
                 </span>
                 <a href="/" className="logo">Rebecca Shi</a>
-                <ul className="main-nav" id="js-menu">
-                    <li><a href="/about" className="nav-links">About</a></li>
-                    <li><a href="/resume" className="nav-links">Resume</a></li>
-                    <li><a href="#" className="nav-links">Portfolio</a></li>
-                </ul>
-            </div>
-        ):
-        (
-            <div id="customNav">
-                <span className="navbar-toggle" id="js-navbar-toggle">
-                    <i className="fas fa-bars" onClick={this.dropdownOnClick}></i>
-                </span>
-                <a href="/" className="logo">Rebecca Shi</a>
-                <ul className="main-nav-gone" id="js-menu">
-                    {/* <li><a href="/" className="nav-links">Home</a></li> */}
+                <ul className={dropdown_class} id="js-menu">
                     <li><a href="/about" className="nav-links">About</a></li>
                     <li><a href="/resume" className="nav-links">Resume</a></li>
                     <li><a href="#" className="nav-links">Portfolio</a></li>
